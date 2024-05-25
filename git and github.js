48bf836aa08f6ac1ed81066bd09055f30d809c6e
MD ZAKIR HOSSAIN <159409026+MDZakirHossain9030@users.noreply.github.com>
2024-05-25 07:24:49 -0700

    Update git and github.js

--- git and github.js
+++ git and github.js
@@ -2,10 +2,11 @@
                                   git & github = SubmitEvent
                                     
 ==>>computere একটা folder create করতে হবে অথবা
-        github এ একটি repositiory(folder) create করতে হবে github এর repository বা folder যদি আমাদের computere আনতে চাই 
-        তাহলে github থেকে url copy (enter) করে এনে  git bash গিয়ে লেখতে হবে =
-                                                                                 git clone(url copy past)
+        github এ একটি repositiory(folder) create করতে হবে github এর repository বা folder টা যদি আমাদের computere আনতে চাই 
+        তাহলে github থেকে url copy করে এনে  git bash গিয়ে লেখতে হবে =
+                                                                      git clone(url copy past) enter
 
  এখন computerer folder অথবা github থেকে নিয়ে আসা folder যেটাতেই কাজ করতে চাই   initialization করতে  হবে
  initialization মানে হলো folder এর দিকে সবসময় চেয়ে থাকবে কোথায় পরিবর্তন হয়েছে  কি পরিবর্তন হয়েছে 
                                                                               git init (enter) দিলেই initialization folder create হয়ে যাবে
+

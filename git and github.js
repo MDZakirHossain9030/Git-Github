095ad9bd77fe6b1adbf510ece2ef9af941ca6549
MD ZAKIR HOSSAIN <159409026+MDZakirHossain9030@users.noreply.github.com>
2024-05-24 21:51:50 -0700

    Update git and github.js

--- git and github.js
+++ git and github.js
@@ -1,1 +1,11 @@
-Git and Gihub
+                                        Git and Gihub
+                                  git & github = SubmitEvent
+                                    
+==>>computere একটা folder create করতে হবে অথবা
+        github এ একটি repositiory(folder) create করতে হবে github এর repository বা folder যদি আমাদের computere আনতে চাই 
+        তাহলে github থেকে url copy (enter) করে এনে  git bash গিয়ে লেখতে হবে =
+                                                                                 git clone(url copy past)
+
+ এখন computerer folder অথবা github থেকে নিয়ে আসা folder যেটাতেই কাজ করতে চাই   initialization করতে  হবে
+ initialization মানে হলো folder এর দিকে সবসময় চেয়ে থাকবে কোথায় পরিবর্তন হয়েছে  কি পরিবর্তন হয়েছে 
+                                                                                                 git init (enter)

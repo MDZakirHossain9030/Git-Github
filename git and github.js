01cc620ba1929f176ce3b97fcdc880bc47d6240d
MD ZAKIR HOSSAIN <159409026+MDZakirHossain9030@users.noreply.github.com>
2025-02-11 04:52:27 -0800

    Update git and github.js

--- git and github.js
+++ git and github.js
@@ -9,14 +9,15 @@
 2>> file এ কি কি আছ তা দেখার জন্য = ls -a 	(-a দিলে hidden file সহ দেখাবে)
 3>> এক file ভেতরে ঢুকতে চাইলে = cd directory-name (enter)
 4>> এক file বাহিরে আসতে চাইলে = cd ../  (enter)
-5>> directory create করার জন্য = mkdir directory-name (enter)
-6>> directory delete করার জন্য = rmdir directory-name (enter)
-7>> file create করার জন্য = touch index.html (enter)
-8>> file delete করার জন্য  = rm index.html (enter0)
-9>> file এর মধ্যে কিছু লেখতে চাইলে = echo "MD Zakir Hossain"> index.html (enter)
-10>> file এর মধ্যে কি লেখা আছে তা দেখার জন্য = cat index.html (enter)
-11>> gitbas cleare করার জন্য = clear or ctrl+l (এল)
-12>> file কোন অবস্থায় আছে সেটা দেখার জন্য = git status
+5>> directory কে git directory করতে চাইলে = git init
+6>> directory create করার জন্য = mkdir directory-name (enter)
+7>> directory delete করার জন্য = rmdir directory-name (enter)
+8>> file create করার জন্য = touch index.html (enter)
+9>> file delete করার জন্য  = rm index.html (enter0)
+10>> file এর মধ্যে কিছু লেখতে চাইলে = echo "MD Zakir Hossain"> index.html (enter)
+11>> file এর মধ্যে কি লেখা আছে তা দেখার জন্য = cat index.html (enter)
+12>> gitbas cleare করার জন্য = clear or ctrl+l (এল)
+13>> file কোন অবস্থায় আছে সেটা দেখার জন্য = git status
 
 
 
@@ -50,25 +51,43 @@
 
 
 
+
+<:BRANCH:>
+1>> কতটা branch আছে তা দেখার জন্য = git branch (enter)
+2>> new branch create করতে চাইলে = git baranch branch-name (enter)
+3>> এক branch থেকে অন্য branch এ যাওয়ার জন্য = git checkout branch-name 	(যে branch এ যেতে চাচ্ছি সেই branch এর নামে)
+4>> branch delete করার জন্য  = git branch -d branch-name (main branch গিয়ে অন্য branch delete করতে হবে)
+5>> এক branch এর লেখা অন্য branch এ নিতে চাইলে মানে marge করতে চাইলে = git merge main -m "যে কোনো মেসেজ" (main branch এ গিয়ে marge করতে হবে তার মানে অন্য branch এর লেখা marge করে main branch এ আনতে চাচ্ছি)
+6>> main branch & developement branch দুইটাইতেই যদি modify করি তাহলে developement branch এর সাথে main branch Auto merge হবে না 													
+			সেই ক্ষেত্রে দুইজনে ফোনে কথা বলে যেকোনো একটা file ঠিক করতে হবে 
+
+
+
+
+
+
+
+
+
+
+				
 <:GITHUB:>
 
-
 						Local repository >>>>>> remote repository push -u origin branch-name
+1>> Local থেকে Remote এ নিতে চাইলে = git push -U origin main ( main মানে main branch এ নিতে চাচ্ছি আর origin মানে হলো remote)
 
-1>> Local repo থেকে github এর কোনো branch এ নিতে চাইলে = git pust -u
 
 
-	
+
+							Local repositiory <<<<<< Remote Local = fetch/pull
+2>> remote repo গোলোকে merge করে একসাথে নিয়ে আসতে চাইলে = git pull (তাহলে আর নতুন করে git merge করতে হবে না সাথে কী কী modify করেছি সেগোলোও দেখতে পারবো)
+		
+
+
 
 									 
-==>>computere একটা folder create করতে হবে অথবা
-        github এ একটি repositiory(folder) create করতে হবে github এর repository বা folder টা যদি আমাদের computere আনতে চাই 
-        তাহলে github থেকে url copy করে এনে  git bash গিয়ে লেখতে হবে =
-                                                                      git clone(url copy past) enter
+==>>computere থেকে কোনো repo local directory তে আনতে চাইলে = git clone(url copy past) 
 
- এখন computerer folder অথবা github থেকে নিয়ে আসা folder যেটাতেই কাজ করতে চাই   initialization করতে  হবে
- initialization মানে হলো folder এর দিকে সবসময় চেয়ে থাকবে কোথায় পরিবর্তন হয়েছে  কি পরিবর্তন হয়েছে 
-                                                                              git init (enter) দিলেই initialization folder create হয়ে যাবে
 
 
 
@@ -97,50 +116,15 @@
 
 
 
-
-
-										Branch--শাখা
-
-1>> আমাদের এ্যাপ্লিকেশনে ( github ) কয়টা branch আছে তা দেখার জন্য =
-							git branch
-2>> New branch create করতে চাইলে =
-			git branch developement (developement নামে একটা নতুন branch create করেছি )নতুন branch create করলে main branch
-			এর সব কিছু copy হয়ে নতুন branchএ চলে যাবে
-3>> এক branch থেকে অন্য branchএ যেতে চাইলে  =
-					git checkout developement
-4>> এক branch এর file  অন্য branch এ নিয়ে আসতে চাই প্রথমে create করা branch এ গিয়ে merge করতে হবে main branch এর সাথে =
-											git merge main -m "যে কোনো মেসেজ"
-আবার checkout করে main branch গিয়ে create করা branch এর সাথে merge করতে হবে 
-
-
-																														
-5>> main branch & developement branch দুইটাইতেই যদি modify করি তাহলে developement branch এর সাথে main branch Auto merge হবে না 													
-			সেই ক্ষেত্রে দুইজনে ফোনে কথা বলে যেকোনো একটা file ঠিক করতে হবে
 			
 
 
 
-								Local repositiory >>>>> Remote = push
-
-1>> Local থেকে Remote এ নিতে চাইলে = 
-			git push origin main ( main মানে main branch এ নিতে চাচ্ছি আর origin মানে হলো remote )
+								
 
 
 
 
 
-							 Local repositiory <<<<<< Remote Local = fetch/pull
-
-1>> remote এর কোনো file এ  modify করেছি এবং Local এ আনতে চাচ্ছি =
-					git fetch এখন git status দিলে শধু changes টা দেখতে পারব কিন্তু কোনো file দেখতে পারব না 
-	দেখার জন্য আবার =
-			git merge করতে হবে 
-	
-
-
-2>> remote file গোলোকে  merge করে একসাথে নিয়ে আসতে চাইলে =
-			git pull তাহলে আর নতুন করে git merge করতে হবে না 
-		এবং 
-		কী কী modify করেছি সেগোলোও দেখতে পারবো
 
 
